1e0a8fa5df15fbdf49e5adf54aad310f3e2101a4
Zeri Aaron <zeriaaron23@gmail.com>
2025-06-21 00:51:35 +0800

    Listed all projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,6 +4,16 @@ function Projects() {
     return (
         <>
             <h1>Projects</h1>
+            <ul>
+                <li>Personal Website</li>
+                <li>Philippine Inform Reading Inventory Automation</li>
+                <li>Spoken</li>
+                <li>Smart Calculator</li>
+                <li>Signal Processing Application</li>
+                <li>Frequency Distribution Table</li>
+                <li>CheckMe!</li>
+                <li>Traffic Lights Pulo Diezmo Road</li>
+            </ul>
         </>
     )
 }
