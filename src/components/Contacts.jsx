e062fde85e6a8c26713bd7d5815d04834c6595cf
Zeri Aaron <zeriaaron23@gmail.com>
2025-06-21 00:46:02 +0800

    Setup the contact form using FormSpree

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,9 +1,22 @@
-
+import { useForm, ValidationError } from "@formspree/react"
 
 function Contacts() {
+    const [state, handleSubmit] = useForm('mzzgagoq')
+
+    if (state.succeded) {
+        return <p>Thanks for contacting!</p>
+    }
+
     return (
         <>
-            <h1>Contacts</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="email">Email Address</label>
+                <input id="email" type="email" name="email" />
+                <ValidationError prefix="Email" field="email" errors={state.errors}/>
+                <textarea name="message" id="message"/>
+                <ValidationError prefix="Message" field="message" errors={state.errors}/>
+                <button type="submit" disabled={state.submitting}>Submit</button>
+            </form>
         </>
     )
 }
